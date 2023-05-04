ed93e281d16cb418e2e328d9831d87c9f08c8e55
joao victor <thehoodgamer20@gmail.com>
2023-05-04 20:09:49 -0300

    finalizado por hj clk kk

--- JS/App.js
+++ JS/App.js
@@ -1,7 +1,7 @@
 function confirm(){
     let usuario = document.getElementById("usuario").value
 
-    alertify.alert('I.A do Guia', `Cadastro realizado com sucesso! Bem vindo(a) ${usuario}`);
+    alertify.alert('I.A do Guia', `Cadastro realizado com sucesso! Bem vindo(a) ${usuario}`, function(){alertify.success('Aproveite!')});
 
 
 }
