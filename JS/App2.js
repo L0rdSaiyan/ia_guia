38437bae990e4c8eec45509d070e0d0b7f0be2e4
joao victor <thehoodgamer20@gmail.com>
2023-05-09 19:33:49 -0300

    iniciado a trabalhar no javascript das mensagens

--- JS/App2.js
+++ JS/App2.js
@@ -1,8 +1,7 @@
 let msgUser = document.getElementById("msgUser");
 
-function sendMsg() {
-  alert(msgUser.value);
-}
+let msgEnviar = document.getElementById("msg-area");
+let btnEnviar = document.getElementById("btn");
 
 document.addEventListener("keydown", function(event) {
   if (event.code === "Enter") {
@@ -14,13 +13,24 @@ document.addEventListener("keydown", function(event) {
   }
 });
 
-let msgEnviar = document.getElementById("msg-area");
-let btnEnviar = document.getElementById("btn");
 
 btnEnviar.addEventListener('click', function() {
   const msgEnviada = document.getElementById("msg-usuario")
+  let chat2 = document.querySelector(".chat-2Inv")
+  let chat3 = document.querySelector(".chat-3Inv")
   msgEnviada.innerText = msgEnviar.value
+  chat2.className = "chat-2"
+
+  setTimeout(()=>{
+
+
+    chat3.className = "chat-3"
+
+  },3000)
+
+
 
 
 });
+
 
