6211554e8da167b0c222c9fccbf0bb0a406c32e0
joao victor <thehoodgamer20@gmail.com>
2023-05-09 19:39:06 -0300

    colocado link redirect para a pagina anterior

--- JS/App2.js
+++ JS/App2.js
@@ -1,36 +1,27 @@
 let msgUser = document.getElementById("msgUser");
-
 let msgEnviar = document.getElementById("msg-area");
 let btnEnviar = document.getElementById("btn");
+const logo = document.getElementById("titulo-circle");
 
-document.addEventListener("keydown", function(event) {
+logo.addEventListener("click", () => {
+  window.location.href = "/index.html";
+});
+
+document.addEventListener("keydown", (event) => {
   if (event.code === "Enter") {
-
-    const msgEnviada = document.getElementById("msg-usuario")
-    msgEnviada.innerText = msgEnviar.value
-  
-
+    const msgEnviada = document.getElementById("msg-usuario");
+    msgEnviada.innerText = msgEnviar.value;
   }
 });
 
+btnEnviar.addEventListener("click", function () {
+  const msgEnviada = document.getElementById("msg-usuario");
+  let chat2 = document.querySelector(".chat-2Inv");
+  let chat3 = document.querySelector(".chat-3Inv");
+  msgEnviada.innerText = msgEnviar.value;
+  chat2.className = "chat-2";
 
-btnEnviar.addEventListener('click', function() {
-  const msgEnviada = document.getElementById("msg-usuario")
-  let chat2 = document.querySelector(".chat-2Inv")
-  let chat3 = document.querySelector(".chat-3Inv")
-  msgEnviada.innerText = msgEnviar.value
-  chat2.className = "chat-2"
-
-  setTimeout(()=>{
-
-
-    chat3.className = "chat-3"
-
-  },3000)
-
-
-
-
+  setTimeout(() => {
+    chat3.className = "chat-3";
+  }, 3000);
 });
-
-
