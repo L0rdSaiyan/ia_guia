dff78ed87760a9b5052c110fba190fa1ca6e9230
joao victor <thehoodgamer20@gmail.com>
2023-05-13 15:23:14 -0300

    modificado o titulo no chat bot

--- JS/App.js
+++ JS/App.js
@@ -8,8 +8,8 @@ Swal.fire({
   '<p>A princípio, pode não parecer que há uma conexão direta entre guia de turismo, robótica e sustentabilidade. No entanto, há maneiras em que esses elementos podem ser combinados para promover práticas sustentáveis ​​e responsáveis ​​no setor do turismo.</p> '+
   '<p>Por exemplo, a robótica pode ser usada para melhorar a eficiência e reduzir o impacto ambiental das atividades turísticas. Robôs podem ser usados para coletar lixo nas praias, monitorar a qualidade do ar e da água, e até mesmo para ajudar na conservação de espécies ameaçadas de extinção.  </p> '+
   '<p>Além disso, guias de turismo podem desempenhar um papel importante na promoção de práticas sustentáveis. Guias podem educar os turistas sobre a importância da preservação ambiental e cultural, incentivando-os a adotar comportamentos mais conscientes durante suas viagens. Eles também podem ajudar a promover atividades turísticas que sejam menos prejudiciais ao meio ambiente e à comunidade local.  </p>'+
-  '<p>Em resumo, a combinação de guias de turismo e robótica pode ser uma forma de promover práticas sustentáveis ​​e responsáveis ​​no setor do turismo, incentivando comportamentos conscientes e a adoção de tecnologias que reduzem o impacto ambiental.</p>' 
-
+  '<p>Em resumo, a combinação de guias de turismo e robótica pode ser uma forma de promover práticas sustentáveis ​​e responsáveis ​​no setor do turismo, incentivando comportamentos conscientes e a adoção de tecnologias que reduzem o impacto ambiental.</p>',
+  confirmButtonText: 'Fechar'
 })
 
 }
